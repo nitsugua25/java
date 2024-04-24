21fd55d02444c7244803ebf1ec9ea138cc3471cf
nitsugua <augustinmichiels@gmail.com>
2024-04-24 21:56:46 +0200

    it's done fastest pattern done it's magic

--- src/TP11/decorator/Client.java
+++ src/TP11/decorator/Client.java
@@ -1,4 +1,15 @@
 package TP11.decorator;
 
 public class Client {
+
+    public static void main(String[] args) {
+
+
+        Pizza classicPizza = new Meatball(new Cheese(new ClassicPizza()));
+
+        System.out.println("Ingredient : "+classicPizza.getDescription());
+
+        System.out.println("Price : "+classicPizza.getCost());
+
+    }
 }
